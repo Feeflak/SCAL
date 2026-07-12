4461ab7bb8bfcf5623bfd9703bfacdf61b3cb0a9
Feeflak <137614590+Feeflak@users.noreply.github.com>
2026-07-12 18:31:55 +0200

    updated flake to work with flake parts

--- flake.nix
+++ flake.nix
@@ -1,111 +1,76 @@
 {
+  description = "SCAL";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  imports = [ ];
 
-  outputs =
+  perSystem =
+    { pkgs, ... }:
+    let
+      runtimeLibs = with pkgs; [
+        ffmpeg
+
+        wayland
+        libxkbcommon
+
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libxcb
+
+        alsa-lib
+        libpulseaudio
+      ];
+
+      runtime = pkgs.rustPlatform.buildRustPackage {
+        pname = "scal-runtime";
+        version = "0.1.0";
+
+        src = ./scal-runtime;
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.clang
+        ];
+
+        buildInputs = runtimeLibs;
+      };
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+      packages = {
+        default = runtime;
+        runtime = runtime;
+      };
 
-        runtime = pkgs.rustPlatform.buildRustPackage {
-          pname = "scal-runtime";
-          version = "0.1.0";
+      apps.default = {
+        type = "app";
+        program = "${runtime}/bin/scal-runtime";
+      };
 
-          src = ./scal-runtime/.;
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.rust-analyzer
+          pkgs.rustfmt
+          pkgs.clippy
+          pkgs.cargo
+          pkgs.rustc
 
-          cargoLock.lockFile = ./Cargo.lock;
+          pkgs.pkg-config
+          pkgs.clang
+          pkgs.llvmPackages.libclang
+          pkgs.alsa-lib.dev
+        ]
+        ++ runtimeLibs;
 
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            clang
-          ];
+        shellHook = ''
+          export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
 
-          buildInputs = with pkgs; [
-            ffmpeg
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
 
-            wayland
-            libxkbcommon
-
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
-            xorg.libxcb
-
-            alsa-lib
-            libpulseaudio
-          ];
-        };
-      in
-      {
-        packages = {
-          default = runtime;
-          runtime = runtime;
-        };
-
-        apps.default = {
-          type = "app";
-          program = "${runtime}/bin/scal-runtime";
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            rust-analyzer
-            rustfmt
-            clippy
-            cargo
-            rustc
-
-            pkg-config
-            ffmpeg
-
-            wayland
-            libxkbcommon
-            xorg.libX11
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXrandr
-            xorg.libxcb
-
-            clang
-            llvmPackages.libclang
-
-            alsa-lib
-            alsa-lib.dev
-            libpulseaudio
-          ];
-
-          shellHook = ''
-            export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
-
-            export LD_LIBRARY_PATH="${
-              pkgs.lib.makeLibraryPath [
-                pkgs.wayland
-                pkgs.libxkbcommon
-                pkgs.xorg.libX11
-                pkgs.xorg.libXcursor
-                pkgs.xorg.libXi
-                pkgs.xorg.libXrandr
-                pkgs.xorg.libxcb
-                pkgs.alsa-lib
-                pkgs.libpulseaudio
-              ]
-            }:$LD_LIBRARY_PATH"
-
-            export PKG_CONFIG_PATH="${pkgs.alsa-lib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-          '';
-        };
-      }
-    );
+          export PKG_CONFIG_PATH="${pkgs.alsa-lib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+        '';
+      };
+    };
 }
