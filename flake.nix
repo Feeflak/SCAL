98e6d348a125b5f8e7ebf3679b9f91684b3350f8
Feeflak <137614590+Feeflak@users.noreply.github.com>
2026-07-12 18:39:28 +0200

    this time the flake will really work

--- flake.nix
+++ flake.nix
@@ -1,76 +1,23 @@
 {
   description = "SCAL";
 
-  imports = [ ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
-  perSystem =
-    { pkgs, ... }:
-    let
-      runtimeLibs = with pkgs; [
-        ffmpeg
-
-        wayland
-        libxkbcommon
-
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXrandr
-        xorg.libxcb
-
-        alsa-lib
-        libpulseaudio
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
-      runtime = pkgs.rustPlatform.buildRustPackage {
-        pname = "scal-runtime";
-        version = "0.1.0";
-
-        src = ./scal-runtime;
-
-        cargoLock.lockFile = ./Cargo.lock;
-
-        nativeBuildInputs = [
-          pkgs.pkg-config
-          pkgs.clang
-        ];
-
-        buildInputs = runtimeLibs;
-      };
-    in
-    {
-      packages = {
-        default = runtime;
-        runtime = runtime;
-      };
-
-      apps.default = {
-        type = "app";
-        program = "${runtime}/bin/scal-runtime";
-      };
-
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.rust-analyzer
-          pkgs.rustfmt
-          pkgs.clippy
-          pkgs.cargo
-          pkgs.rustc
-
-          pkgs.pkg-config
-          pkgs.clang
-          pkgs.llvmPackages.libclang
-          pkgs.alsa-lib.dev
-        ]
-        ++ runtimeLibs;
-
-        shellHook = ''
-          export LIBCLANG_PATH="${pkgs.llvmPackages.libclang.lib}/lib"
-
-          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
-
-          export PKG_CONFIG_PATH="${pkgs.alsa-lib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-        '';
-      };
+      imports = [
+        ./flake-module.nix
+      ];
     };
 }
